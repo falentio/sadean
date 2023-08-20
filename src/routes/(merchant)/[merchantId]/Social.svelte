9af92212579476e5c9e647a9efc40b9a7f934197
falentio <riv.kevinfalent.io@gmail.com>
2023-08-20 22:39:18 +0700

    init

--- src/routes/(merchant)/[merchantId]/Social.svelte
+++ src/routes/(merchant)/[merchantId]/Social.svelte
@@ -9,25 +9,25 @@
 	class="flex flex-wrap flex-col text-xl justify-center items-stretch [&>*]:(inline-flex flex-row gap-2) gap-6 max-w-md mx-auto"
 >
 	{#if owner.facebook}
-		<Button class="bg-blue-5">
+		<Button class="bg-blue-5" href="//facebook.com/{owner.facebook}">
 			<span class="i-mdi:facebook w-8" />
 			Facebook
 		</Button>
 	{/if}
 	{#if owner.instagram}
-		<Button class="bg-rose-5">
+		<Button class="bg-rose-5" href="//instagram.com/{owner.instagram}">
 			<span class="i-mdi:instagram w-8" />
 			Instagram
 		</Button>
 	{/if}
 	{#if owner.twitter}
-		<Button class="bg-lightblue-5">
+		<Button class="bg-lightblue-5" href="//twitter.com/{owner.twitter}">
 			<span class="i-mdi:twitter w-8" />
 			Twitter
 		</Button>
 	{/if}
 	{#if owner.whatsapp}
-		<Button class="bg-green-5">
+		<Button class="bg-green-5" href="//wa.me/{owner.whatsapp}">
 			<span class="i-mdi:whatsapp w-8" />
 			Whatsapp
 		</Button>
